b557d50a8a5170e9f7b30ddaafba2e6d616d3d55
Lucas Estienne <lucas.estienne@cira.ca>
2019-07-20 15:32:22 -0400

    mud cli

--- muddy/constants.py
+++ muddy/constants.py
@@ -2,8 +2,8 @@
 MUD_MODEL_DEF = {
     "mud-version": "This node specifies the integer version of the MUD specification.",
     "mud-url": "This URL identifies the MUD file.",
-    "to-device-policy": "indicates the appropriate direction of a flow in association with a Thing",
-    "from-device-policy": "indicates the appropriate direction of a flow in association with a Thing",
+    "to-device-policy": "The policies that should be enforced on traffic going to the device.",
+    "from-device-policy": "The policies that should be enforced on traffic coming from the device.",
     "last-update": "This is a date-and-time value of when the MUD file was generated.",
     "cache-validity": "This uint8 is the period of time in hours that a network management station MUST wait since its last retrieval before checking for an update.",
     "is-supported": "This boolean is an indication from the manufacturer to the network administrator as to whether or not the Thing is supported. ",
@@ -11,5 +11,7 @@ MUD_MODEL_DEF = {
     "mfg-name": "These optional fields are filled in as specified by [RFC8348]",
     "software-rev": "These optional fields are filled in as specified by [RFC8348]",
     "model-name": "These optional fields are filled in as specified by [RFC8348]",
-    "firmware-rev": "These optional fields are filled in as specified by [RFC8348]"
+    "firmware-rev": "These optional fields are filled in as specified by [RFC8348]",
+    "documentation": "This URI consists of a URL that points to documentation relating to the device and the MUD file.",
+    "extensions": "A list of extension names that are used in this MUD file.  Each name is registered with the IANA and described in an RFC."
 }
--- muddy/scripts/mudcli.py
+++ muddy/scripts/mudcli.py
@@ -6,9 +6,18 @@ def cli():
     pass
 
 @cli.command()
-@click.option('--version', default=1, help=MUD_MODEL_DEF["mud-version"], show_default=True)
-@click.option('--mud-url', help=MUD_MODEL_DEF["mud-url"])
+@click.option('--mud-version', '-v', required=True, default=1, help=MUD_MODEL_DEF["mud-version"], show_default=True)
+@click.option('--mud-url', '-u', required=True, help=MUD_MODEL_DEF["mud-url"])
+@click.option('--is-supported', '-s', required=True, type=click.BOOL, default=True, help=MUD_MODEL_DEF["is-supported"], show_default=True)
 @click.option('--cache-validity', default=48, help=MUD_MODEL_DEF["cache-validity"], show_default=True)
-@click.option('--is-supported/--is-not-supported', default=True, help=MUD_MODEL_DEF["is-supported"], show_default=True)
-def make(version, mud_url, cache_validity, is_supported):
+@click.option('--systeminfo', help=MUD_MODEL_DEF["systeminfo"])
+@click.option('--documentation', help=MUD_MODEL_DEF["documentation"])
+@click.option('--mfg-name', help=MUD_MODEL_DEF["mfg-name"])
+@click.option('--model-name', help=MUD_MODEL_DEF["model-name"])
+@click.option('--firmware-rev', help=MUD_MODEL_DEF["firmware-rev"])
+@click.option('--software-rev', help=MUD_MODEL_DEF["software-rev"])
+@click.option('--extensions', help=MUD_MODEL_DEF["extensions"])
+def make(mud_version, mud_url, cache_validity, is_supported, systeminfo,
+        mfg_name, documentation, model_name, firmware_rev, software_rev,
+        extensions):
     click.echo('make mud file')
