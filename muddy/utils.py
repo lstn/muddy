6a8fe74f26b21c5587634912c387635d8f553095
Daniel Innes <Daniel.innes@cira.ca>
2019-07-21 16:03:02 -0400

    Fix mistake

--- muddy/utils.py
+++ muddy/utils.py
@@ -62,42 +62,42 @@ def get_sub_ace_name(ace_name, direction):
 
 
 def get_ipversion_object(ip_version):
-    if ip_version is 'ipv4':
+    if ip_version == 'ipv4':
         return IPVersion.IPV4
-    if ip_version is 'ipv6':
+    if ip_version == 'ipv6':
         return IPVersion.IPV4
-    if ip_version is 'both':
+    if ip_version == 'both':
         return IPVersion.BOTH
     raise InputException(f'ip_version is not valid: {ip_version}')
 
 
 def get_protocol_object(protocol):
-    if protocol is 'udp':
+    if protocol == 'udp':
         return Protocol.UDP
-    if protocol is 'tcp':
+    if protocol == 'tcp':
         return Protocol.TCP
-    if protocol is 'any':
+    if protocol == 'any':
         return Protocol.ANY
     raise InputException(f'protocol is not valid: {protocol}')
 
 
 def get_direction_object(direction):
-    if direction is 'to_device':
+    if direction == 'to_device':
         return Direction.TO_DEVICE
-    if direction is 'from_device':
+    if direction == 'from_device':
         return Direction.FROM_DEVICE
     raise InputException(f'direction is not valid: {direction}')
 
 
 def get_match_type_object(match_type):
-    if match_type is 'is_my_controller':
+    if match_type == 'is_my_controller':
         return MatchType.IS_MY_CONTROLLER
-    if match_type is 'is_controller':
+    if match_type == 'is_controller':
         return MatchType.IS_CONTROLLER
-    if match_type is 'is_mfg':
+    if match_type == 'is_mfg':
         return MatchType.IS_MFG
-    if match_type is 'is_mymfg':
+    if match_type == 'is_mymfg':
         return MatchType.IS_MYMFG
-    if match_type is 'is_cloud':
+    if match_type == 'is_cloud':
         return MatchType.IS_CLOUD
     raise InputException(f'match_type is not valid: {match_type}')
