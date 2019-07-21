40d684567b855858efefef189e2bff2baaa44241
Lucas Estienne <lucas.estienne@cira.ca>
2019-07-20 20:38:57 -0400

    setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,13 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name='muddy',
     version='2019.7.20',
+    author='Lucas Estienne, Daniel Innes',
+    author_email='lucas@estienne.sh, daniel.w.innes@gmail.com',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
@@ -12,4 +17,12 @@ setup(
         [console_scripts]
         muddy=muddy.scripts.mudcli:cli
     ''',
+    url="https://github.com/lstn/muddy",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
 )
